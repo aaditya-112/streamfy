73be487f7e4701549f5d37213b134a46e244476a
aaditya-112 <121390012+aaditya-112@users.noreply.github.com>
2026-02-20 23:55:12 +0530

    completed ui of onboarding page

--- frontend/src/pages/OnbordingPage.jsx
+++ frontend/src/pages/OnbordingPage.jsx
@@ -3,7 +3,7 @@ import useAuthUser from "../hooks/useAuthUser";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { completeOnboarding } from "../lib/api";
-import { CameraIcon, ShuffleIcon } from "lucide-react";
+import { CameraIcon, LoaderIcon, MapPinIcon, ShipWheelIcon, ShuffleIcon } from "lucide-react";
 import { LANGUAGES } from "../constants";
 
 const OnbordingPage = () => {
@@ -40,18 +40,17 @@ const OnbordingPage = () => {
     <div className="min-h-screen bg-base-100 flex items-center justify-center p-4">
       <div className="card bg-base-200 w-full max-w-3xl shadow-xl">
         <div className="card-body p-6 sm:p-8">
-          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6">
-            Complete Your profile
-          </h1>
-          <form onSubmit={handleSubmit} className="spaace-y-6">
-            {/* profile pic container */}
+          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6">Complete Your Profile</h1>
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+            {/* PROFILE PIC CONTAINER */}
             <div className="flex flex-col items-center justify-center space-y-4">
-              {/* image preview */}
-              <div className="size-32 rounded-full bg-neutral-content overflow-hidden">
+              {/* IMAGE PREVIEW */}
+              <div className="size-32 rounded-full bg-base-300 overflow-hidden">
                 {formState.profilePic ? (
                   <img
                     src={formState.profilePic}
-                    alt="profile preview"
+                    alt="Profile Preview"
                     className="w-full h-full object-cover"
                   />
                 ) : (
@@ -61,80 +60,120 @@ const OnbordingPage = () => {
                 )}
               </div>
 
-              {/* generate random avataar btn */}
+              {/* Generate Random Avatar BTN */}
               <div className="flex items-center gap-2">
-                <button
-                  className="btn btn-accent"
-                  type="button"
-                  onClick={handleRandomAvatar}
-                >
-                  <ShuffleIcon className="size-4 mr-2" /> Generate Random Avatar
+                <button type="button" onClick={handleRandomAvatar} className="btn btn-accent">
+                  <ShuffleIcon className="size-4 mr-2" />
+                  Generate Random Avatar
                 </button>
               </div>
             </div>
 
-            {/* full name */}
+            {/* FULL NAME */}
             <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Full Name</span>
-                </label>
-                <input
-                  type="text"
-                  name="fullName"
-                  value={formState.fullName}
-                  onChange={(e) =>
-                    setFormState({ ...formState, fullName: e.target.value })
-                  }
-                  className="input input-bordered w-full"
-                  placeholder="Your full name"
-                />
+              <label className="label">
+                <span className="label-text">Full Name</span>
+              </label>
+              <input
+                type="text"
+                name="fullName"
+                value={formState.fullName}
+                onChange={(e) => setFormState({ ...formState, fullName: e.target.value })}
+                className="input input-bordered w-full"
+                placeholder="Your full name"
+              />
             </div>
 
-            {/* bio */}
+            {/* BIO */}
             <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Bio</span>
-                </label>
-                <input
-                  type="text"
-                  name="bio"
-                  value={formState.bio}
-                  onChange={(e) =>
-                    setFormState({ ...formState, bio: e.target.value })
-                  }
-                  className="textarea textarea-boadered h-24 w-full" 
-                  placeholder="Tell others about yourself and language learning goals"
-                />
+              <label className="label">
+                <span className="label-text">Bio</span>
+              </label>
+              <textarea
+                name="bio"
+                value={formState.bio}
+                onChange={(e) => setFormState({ ...formState, bio: e.target.value })}
+                className="textarea textarea-bordered h-24 w-full"
+                placeholder="Tell others about yourself and your language learning goals"
+              />
             </div>
-            
-            {/* language */}
-            <div className="grid grid-cols-1 md:grid-col-2 gap-4">
-              {/* native language */}
+
+            {/* LANGUAGES */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {/* NATIVE LANGUAGE */}
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Native Language</span>
                 </label>
-                <select name="nativeLanguage" value={formState.nativeLanguage} onChange={(e)=>setFormState({...formState, nativeLanguage: e.target.value})} className="select select-bordered w-full">
+                <select
+                  name="nativeLanguage"
+                  value={formState.nativeLanguage}
+                  onChange={(e) => setFormState({ ...formState, nativeLanguage: e.target.value })}
+                  className="select select-bordered w-full"
+                >
                   <option value="">Select your native language</option>
-                  {LANGUAGES.map((lang)=>(
-                    <option key={`native-${lang}`} value={lang.toLowerCase()}>{lang}</option>
+                  {LANGUAGES.map((lang) => (
+                    <option key={`native-${lang}`} value={lang.toLowerCase()}>
+                      {lang}
+                    </option>
                   ))}
                 </select>
               </div>
 
-              {/* learning language */}
+              {/* LEARNING LANGUAGE */}
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Learning Language</span>
                 </label>
-                <select name="nativeLanguage" value={formState.learningLanguage} onChange={(e)=>setFormState({...formState, learningLanguage: e.target.value})} className="select select-bordered w-full">
+                <select
+                  name="learningLanguage"
+                  value={formState.learningLanguage}
+                  onChange={(e) => setFormState({ ...formState, learningLanguage: e.target.value })}
+                  className="select select-bordered w-full"
+                >
                   <option value="">Select language you're learning</option>
-                  {LANGUAGES.map((lang)=>(
-                    <option key={`native-${lang}`} value={lang.toLowerCase()}>{lang}</option>
+                  {LANGUAGES.map((lang) => (
+                    <option key={`learning-${lang}`} value={lang.toLowerCase()}>
+                      {lang}
+                    </option>
                   ))}
                 </select>
               </div>
             </div>
+
+            {/* LOCATION */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Location</span>
+              </label>
+              <div className="relative">
+                <MapPinIcon className="absolute top-1/2 transform -translate-y-1/2 left-3 size-5 text-base-content opacity-70" />
+                <input
+                  type="text"
+                  name="location"
+                  value={formState.location}
+                  onChange={(e) => setFormState({ ...formState, location: e.target.value })}
+                  className="input input-bordered w-full pl-10"
+                  placeholder="City, Country"
+                />
+              </div>
+            </div>
+
+            {/* SUBMIT BUTTON */}
+
+            <button className="btn btn-primary w-full" disabled={isPending} type="submit">
+              {!isPending ? (
+                <>
+                  <ShipWheelIcon className="size-5 mr-2" />
+                  Complete Onboarding
+                </>
+              ) : (
+                <>
+                  <LoaderIcon className="animate-spin size-5 mr-2" />
+                  Onboarding...
+                </>
+              )}
+            </button>
           </form>
         </div>
       </div>
